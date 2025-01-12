a4d84e2903cd43d975e7ec2ce1a2aaa9a7fa796e
DannyDai12138 <108012761+DannyDai12138@users.noreply.github.com>
2025-01-12 20:54:28 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,8 +47,8 @@ const Home: NextPage = () => {
       <Header />
       
       <div className="relative mt-8" style={{top: '-40px'}}>
-        <div className="absolute top-0 text-center w-full px-4 py-2 bg-gray-800 text-white">
-          <h2 className="text-xl font-bold">Important Notice</h2>
+        <div className="absolute top-0 flex items-center justify-center text-center w-full px-4 py-2 bg-gray-800 text-white">
+          <h2 className="text-xl font-bold mr-4">Important Notice</h2>
           <p className="text-sm">Our project is, and will always be, unrelated to any cryptocurrency. Please beware of scams.</p>
         </div>
       </div>
