be85a067dbfe7578712135677edd70b2b7d1b2ef
DannyDai12138 <108012761+DannyDai12138@users.noreply.github.com>
2025-03-05 16:08:51 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,8 +45,19 @@ const Home: NextPage = () => {
         <title>General Scenario Intelligent High-precision Parsing</title>
       </Head>
       <Header />
+      <div className="w-full mt-8">
+        <div className="flex items-center justify-center text-center w-full px-4 py-2 bg-gray-800 text-white mb-0">
+          <h2 className="text-xl font-bold mr-4 whitespace-nowrap">Important Notice</h2>
+          <p className="text-sm whitespace-nowrap">Our project is, and will always be, unrelated to any cryptocurrency. Please beware of scams.</p>
+        </div>
+      </div>
       
-        <div className="relative mt-8 w-full" style={{top: '-40px'}}>
+      <div className="relative-container flex flex-col gap-0">
+        <ImageCarousel images={leftImages} direction="left" />
+        <ImageCarousel images={rightImages} direction="right" />
+        <TextTiltAnimation />
+      </div>
+{/*         <div className="relative mt-8 w-full" style={{top: '-40px'}}>
           <div className="flex items-center justify-center text-center w-full px-4 py-2 bg-gray-800 text-white">
             <h2 className="text-xl font-bold mr-4 whitespace-nowrap">Important Notice</h2>
             <p className="text-sm whitespace-nowrap">Our project is, and will always be, unrelated to any cryptocurrency. Please beware of scams.</p>
@@ -57,7 +68,7 @@ const Home: NextPage = () => {
         <ImageCarousel images={leftImages} direction="left" />
         <ImageCarousel images={rightImages} direction="right" />
         <TextTiltAnimation />
-      </div>
+      </div> */}
 
       <div id="support" ref={supportRef} className="" style={{top: '100px'}}>
         <Support />
