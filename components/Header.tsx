5b1fe749fd8f98a30dbe09e318096c65ae8da23a
DannyDai12138 <108012761+DannyDai12138@users.noreply.github.com>
2025-03-05 17:01:44 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -103,7 +103,7 @@ const Header: React.FC = () => {
             </Link>
           </NavbarBrand>
           <Link
-            href="/segment"
+            href="https://fal.ai/models/fal-ai/birefnet/v2"
             className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white px-5 py-1.5 rounded-md shadow-md hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out ml-4"
           >
             Start
@@ -127,7 +127,7 @@ const Header: React.FC = () => {
           <div className="flex items-center">
             <NavbarItem className="ml-4">
               <Link
-                href="/segment"
+                href="https://fal.ai/models/fal-ai/birefnet/v2"
                 className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white px-5 py-1.5 rounded-md shadow-md hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out"
               >
                 Start
